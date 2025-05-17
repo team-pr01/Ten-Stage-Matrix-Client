76bacf78df9116f71bb773feafc7ee7d4b259488
RAHUL <rahulsd380@gmail.com>
2025-05-17 10:04:49 +0600

    Installed tailwind and setup routing

---
+++ src/routes/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout/MainLayout";
+import NotFound from "../pages/NotFound/NotFound";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
