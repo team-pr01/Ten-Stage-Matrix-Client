c943a9c9bef928a35ee3e298bfb4a6191513ceeb
rahulsd380 <rahulsd380@gmail.com>
2025-07-06 21:55:33 +0600

    Refactored layut in dashboard

--- src/pages/Dashboard/DashboardHome/DashboardHome.tsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.tsx
@@ -76,7 +76,7 @@ const DashboardHome = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mb-5">
         <DashboardCard
           direction="col"
           icon={ICONS.donation}
@@ -109,7 +109,9 @@ const DashboardHome = () => {
               : "0.00000"
           }
         />
+      </div>
 
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
         <DashboardCard
           direction="row"
           icon={ICONS.withdraw}
