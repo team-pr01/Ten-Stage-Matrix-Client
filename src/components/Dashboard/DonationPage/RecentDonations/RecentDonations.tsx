978afe65b141bad5b3ee2448d4d5e2afebaa7bcd
rahulsd380 <rahulsd380@gmail.com>
2025-07-11 16:27:22 +0600

    Added donation table headers

--- src/components/Dashboard/DonationPage/RecentDonations/RecentDonations.tsx
+++ src/components/Dashboard/DonationPage/RecentDonations/RecentDonations.tsx
@@ -1,63 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useGetDonationHistoryQuery } from "../../../../redux/Features/User/userApi";
 import { formatDate } from "../../../../utile/formatDate";
-import Loader from "../../../Shared/Loader/Loader";
+import Table from "../../../Reusable/Table/Table";
 
 const RecentDonations = () => {
   const { data, isLoading } = useGetDonationHistoryQuery({});
   return (
-      <div 
-       style={{
-        boxShadow: "inset 4px 4px 33.2px 0px rgba(255, 255, 255, 0.20)",
-        backdropFilter: "blur(5.05px)",
-      }}
-      className="rounded-[28px] border-2 border-neutral-155 bg-neutral-155 flex flex-col py-7 px-[34px] font-Outfit w-full my-12">
-        <h1 className="text-2xl font-medium text-white">Recent Donations</h1>
-
-        <div className="overflow-x-auto custom-scrollbar mt-6">
-          <table className="w-full text-white">
-            {isLoading ? (
-              <Loader size="size-10" />
-            ) : (
-              <tbody>
-                {data?.data?.donations?.length < 1 ? (
-                  <p className="text-white">No donations found.</p>
-                ) : (
-                  data?.data?.donations?.map((item: any, index: number) => (
-                    <tr key={index} className="border-b border-neutral-110">
-                      <td className="py-3 whitespace-nowrap">{item?._id}</td>
-                      {/* Type + Icon */}
-                      <td className="flex items-center gap-2 py-3 whitespace-nowrap">
-                        {item?.icon && (
-                          <div className="bg-neutral-105 size-[14px] rounded-full p-[3px] flex items-center justify-center">
-                            <img src={item?.icon} alt="" className="size-2" />
-                          </div>
-                        )}
-                        {item?.type && <span>{item?.type}</span>}
-                      </td>
-
-                      {/* Date */}
-                      <td className="py-3 whitespace-nowrap">
-                        {formatDate(item?.created_at)}
-                      </td>
-
-                      {/* Amount */}
-                      <td className="py-3 whitespace-nowrap">
-                        ${item?.amount}
-                      </td>
-
-                      {/* Status */}
-                      <td className="py-3 whitespace-nowrap capitalize">
-                        {item?.status}
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            )}
-          </table>
-        </div>
-      </div>
+    <div className="mt-16">
+      <Table
+        title="Withdrawal Details"
+        tableHeaders={[
+          "Serial No",
+          "Transaction Id",
+          "Type",
+          "Date",
+          "Amount",
+          "Status",
+        ]}
+        data={
+          data?.data?.donations?.map((item: any, index: number) => ({
+            serial_no: index + 1,
+            transaction_id: item._id,
+            type: item.type,
+            date: formatDate(item?.created_at),
+            amount: `$${item?.amount.toFixed(5)}`,
+            status:
+              item?.status === 1
+                ? "Completed"
+                : item?.status === 2
+                ? "Pending"
+                : item?.status === 3
+                ? "Rejected"
+                : item?.status === 4
+                ? "Processing"
+                : "",
+          })) || []
+        }
+        isLoading={isLoading}
+      />
+    </div>
   );
 };
 
