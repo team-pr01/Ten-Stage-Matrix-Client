2cde4e007048182db15e8cbc8d807adc146b4dc1
rahulsd380 <rahulsd380@gmail.com>
2025-07-28 20:54:39 +0600

    Changed api url

--- src/redux/API/baseApi.ts
+++ src/redux/API/baseApi.ts
@@ -9,6 +9,7 @@ import type { DefinitionType } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://api.tsmnews.com/api",
+  // baseUrl: "https://test.adamocorporationltd.com/api",
   // baseUrl: "https://api.adamocorporationltd.com/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
