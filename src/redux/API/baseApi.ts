7914613811ae2e5c1deeb6e43cd04e77c30f9209
rahulsd380 <rahulsd380@gmail.com>
2025-07-28 21:02:13 +0600

    Changed base url to test

--- src/redux/API/baseApi.ts
+++ src/redux/API/baseApi.ts
@@ -8,8 +8,8 @@ import type { RootState } from "../store";
 import type { DefinitionType } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://api.tsmnews.com/api",
-  // baseUrl: "https://test.adamocorporationltd.com/api",
+  // baseUrl: "https://api.tsmnews.com/api",
+  baseUrl: "https://test.adamocorporationltd.com/api",
   // baseUrl: "https://api.adamocorporationltd.com/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
